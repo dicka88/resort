ed732a1a82142924f6cec146d5d051443a98efbc
Dicka Ismaji <dickaismaji@gmail.com>
2023-07-19 09:46:40 +0700

    fix: re-render

--- src/scripts/components/fab-favorite-restaurant.js
+++ src/scripts/components/fab-favorite-restaurant.js
@@ -15,8 +15,10 @@ class FabFavoriteRestaurant extends HTMLElement {
   async connectedCallback() {
     const isExist = await FavoriteRestaurantDb.getRestaurant(this.id);
 
+    const ariaLabel = isExist ? 'Unlike this restaurant' : 'Like this restaurant';
+
     this.innerHTML = `
-      <button class="fab ${isExist ? 'fab-primary' : ''}">
+      <button id="like-button" class="fab ${isExist ? 'fab-primary' : ''}" aria-label="${ariaLabel}">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M13.12 2.06L7.58 7.6c-.37.37-.58.88-.58 1.41V19c0 1.1.9 2 2 2h9c.8 0 1.52-.48 1.84-1.21l3.26-7.61C23.94 10.2 22.49 8 20.34 8h-5.65l.95-4.58c.1-.5-.05-1.01-.41-1.37c-.59-.58-1.53-.58-2.11.01zM3 21c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2s-2 .9-2 2v8c0 1.1.9 2 2 2z"/></svg>
       </button>
     `;
@@ -28,11 +30,9 @@ class FabFavoriteRestaurant extends HTMLElement {
     const isExist = await FavoriteRestaurantDb.getRestaurant(this.id);
 
     if (isExist) {
-      this.querySelector('button').classList.remove('fab-primary');
-      FavoriteRestaurantDb.deleteRestaurant(this.id);
+      await FavoriteRestaurantDb.deleteRestaurant(this.id);
     } else {
-      this.querySelector('button').classList.add('fab-primary');
-      FavoriteRestaurantDb.putRestaurant({
+      await FavoriteRestaurantDb.putRestaurant({
         id: this.id,
         pictureId: this.pictureId,
         name: this.name,
@@ -41,6 +41,8 @@ class FabFavoriteRestaurant extends HTMLElement {
         description: this.description,
       });
     }
+
+    this.connectedCallback();
   }
 }
 
